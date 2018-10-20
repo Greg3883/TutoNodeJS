7f1168e8e9b8e6f8a96d1e675ca14ddc5291fa56
Greg3883 <gregoire.bejjaji@gmail.com>
2018-10-20 16:56:03 +0200

    Création Evenements

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+//MES MODULES
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
+var EventEmitter = require('events').EventEmitter;
 
+//CREATION SERVEUR
 var server = http.createServer(function(req, res) {
     var params = querystring.parse(url.parse(req.url).query);
     res.writeHead(200, {"Content-Type": "text/html"});
@@ -9,8 +12,25 @@ var server = http.createServer(function(req, res) {
         res.end('<p>Bonjour <strong>'+params['prenom']+' '+params['nom']+'</strong> !</p>');
     } else {
         res.end('<p>Qui est tu ?</p>');
-
     }
 });
 
+
+//VARIABLES
+var jeu = new EventEmitter();
+
+
+//EVENEMENTS
+server.on('close', function() {
+    console.log('Au revoir.');
+});
+
+jeu.on('gameover', function(message) {
+    console.log(message);
+});
+
+
+//ACTIONS
+jeu.emit('gameover', 'Vous avez perdu !');
 server.listen(8080);
+
